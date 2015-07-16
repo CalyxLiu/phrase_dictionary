858a1ab8df0600f486570a4a68414527bec331e3
Frankie <frankie.marchan@gmail.com>
2015-07-16 17:59:04 +0000

    It now works but is not pretty yet

--- app_controller.rb
+++ app_controller.rb
@@ -1,6 +1,6 @@
-#require 'bundler'
+require 'bundler'
 require_relative 'models/phrase.rb'
-#Bundler.require
+Bundler.require
 
 class PhraseApp < Sinatra::Base
 
@@ -8,14 +8,44 @@ class PhraseApp < Sinatra::Base
     erb :index
   end
   
-  #get '/about' do
-    #erb :about
-  #end
+  get '/english' do
+    erb :english
+  end
+  
+  get '/spanish' do
+    erb :spanish
+  end
+  
+  get '/french' do
+    erb :french
+  end
+ 
+  post '/eng-result' do
+    primary_lang("English")
+    second_lang(params[:language])
+    @answer = phrase_choice(params[:phrases])
+    erb :eng_result
+  end
+  
+   post '/fra-result' do
+    primary_lang("Français")
+    second_lang(params[:language])
+    @answer = phrase_choice(params[:phrases])
+    erb :fra_result
+  end
+  
+  post '/esp-result' do
+    primary_lang("Español")
+    second_lang(params[:language])
+    @answer = phrase_choice(params[:phrases])
+    erb :esp_result
+  end
   
   #post '/result' do
     #@result_cat = meow_maker(params[:cat_string])
     #@result_dog = bark_maker(params[:dog_string])
     #erb :result
   #end
+  
 
 end
